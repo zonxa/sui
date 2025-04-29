6ab6a6b94b0a26c6c7e7d8d6f8da143f5ed418ed
Ashok Menon <ashok@mystenlabs.com>
2025-04-29 15:50:51 +0100

    fixup: undo kv_packages index change

--- crates/sui-indexer-alt-schema/migrations/2025-04-20-224820_kv_packages/up.sql
+++ crates/sui-indexer-alt-schema/migrations/2025-04-20-224820_kv_packages/up.sql
@@ -15,9 +15,6 @@ ON kv_packages (cp_sequence_number, original_id, package_version);
 CREATE INDEX IF NOT EXISTS kv_packages_id_version_cp
 ON kv_packages (original_id, package_version, cp_sequence_number);
 
-CREATE INDEX IF NOT EXISTS kv_packages_id_cp_version
-ON kv_packages (original_id, cp_sequence_number DESC, package_version DESC);
-
 CREATE INDEX IF NOT EXISTS kv_packages_system_packages
 ON kv_packages (cp_sequence_number, original_id, package_version)
 WHERE is_system_package = true;
